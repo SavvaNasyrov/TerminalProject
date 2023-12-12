56da40d25df7689a10fb564ba024c5473164829e
Савва Насыров <vasyanasyrov06288@gmail.com>
2023-12-12 14:05:32 +0500

    Builder pattern mistake fixed

--- TerminalHelper/Builder/BasicTerminalBuilder.cs
+++ TerminalHelper/Builder/BasicTerminalBuilder.cs
@@ -4,5 +4,30 @@ namespace Building_Console_Terminal_Helper.Builder;
 
 public class BasicTerminalBuilder : TerminalBuilder
 {
-    
+    public void AddNewCommand(string command, DelegatesRepo.TerminalCommand action, string description)
+    {
+        Terminal.TerminalCommands[command] = action;
+        Terminal.Descriptions[command] = description;
+    }
+
+    public void SetPreRunMessage(string message)
+    {
+        Terminal.PreMessage = message;
+    }
+
+    public void SetBreakCondition(DelegatesRepo.Condition condition)
+    {
+        Terminal.Condition = condition;
+    }
+
+    public void UseOfficial()
+    {
+        Terminal.UseOfficial = true;
+    }
+
+    public void DontUseOfficial()
+    {
+        Terminal.UseOfficial = false;
+    }
+
 }
--- TerminalHelper/Builder/TerminalBuilder.cs
+++ TerminalHelper/Builder/TerminalBuilder.cs
@@ -3,33 +3,7 @@
 public abstract class TerminalBuilder
 {
     protected Terminal Terminal = new();
-
-    public void AddNewCommand(string command, DelegatesRepo.TerminalCommand action, string description)
-    {
-        Terminal.TerminalCommands[command] = action;
-        Terminal.Descriptions[command] = description;
-    }
-
-    public void SetPreRunMessage(string message)
-    {
-        Terminal.PreMessage = message;
-    }
-
-    public void SetBreakCondition(DelegatesRepo.Condition condition)
-    {
-        Terminal.Condition = condition;
-    }
-
-    public void UseOfficial()
-    {
-        Terminal.UseOfficial = true;
-    }
-
-    public void DontUseOfficial()
-    {
-        Terminal.UseOfficial = false;
-    }
-
+    
     public Terminal Build()
     {
         return Terminal;
